2e195396b77cb5efe692841f88e63d625089ec52
Nelson Arevalo <nelsonarevalo0696@gmail.com>
2024-07-29 19:50:26 -0500

    Create: Adding tailwind and footer

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    FooterComponent
+  ],
+  exports: [
+    FooterComponent
   ]
 })
 export class ComponentsModule { }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    screens: {
+      'min-1320': {min: '1320px'},
+      'min-1200': {min: '1200px'},
+      'min-992': {min: '992px'},
+      'min-576': {min: '576px'},
+    },
+    extend: {
+      fontFamily: {
+        'open-sans': "'Open Sans', sans-serif"
+      },
+    },
   },
   plugins: [],
 }
